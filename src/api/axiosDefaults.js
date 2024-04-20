327aacb53cccfde98e8b7f8f94d3d081d18fc2d2
elindalenback <elin.dalenback@gmail.com>
2024-04-20 10:10:55 +0000

    Implement axios interceptors for token refresh and error handling

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,5 +4,6 @@ axios.defaults.baseURL = "https://brain-box-c3ea3510445f.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
+
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
