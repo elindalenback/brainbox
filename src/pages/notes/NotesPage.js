9b1d6465e96b51b438418aee73f787aed6badfb1
elindalenback <elin.dalenback@gmail.com>
2024-04-20 17:30:32 +0000

    Ad the searchbar and set up the search functionallity

--- src/pages/notes/NotesPage.js
+++ src/pages/notes/NotesPage.js
@@ -14,16 +14,20 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function NotesPage({ message, filter = "" }) {
   const [notes, setNotes] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const { data } = await axiosReq.get(`/notes/?${filter}`);
+        const { data } = await axiosReq.get(`/notes/?${filter}search=${query}`);
         setNotes(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,19 +36,45 @@ function NotesPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchNotes();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchNotes();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search notes"
+          />
+        </Form>
         {hasLoaded ? (
           <>
             {notes.results.length ? (
-              notes.results.map((note) => (
-                <Note key={note.id} {...note} setNotes={setNotes} />
-              ))
+              <InfiniteScroll
+                children={notes.results.map((note) => (
+                  <Note key={note.id} {...note} setNotes={setNotes} />
+                ))}
+                dataLength={notes.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!notes.next}
+                next={() => fetchMoreData(notes, setNotes)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
