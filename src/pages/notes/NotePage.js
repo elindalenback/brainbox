08a1e4f1cd1806668b8a2a9a85db9dc9ef61fc0b
elindalenback <elin.dalenback@gmail.com>
2024-04-21 11:09:41 +0000

    Add MoreDropdown menu to comments and implement deleting comment

--- src/pages/notes/NotePage.js
+++ src/pages/notes/NotePage.js
@@ -58,7 +58,12 @@ function NotePage() {
           ) : null}
           {comments.results.length ? (
             comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
+              <Comment
+                key={comment.id}
+                {...comment}
+                setNote={setNote}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
