72bfc1105f16931b3e8631abb932c1fe4e15f7bd
elindalenback <elin.dalenback@gmail.com>
2024-04-21 13:33:57 +0000

    Add popular profiles for big screens and mobile

--- src/pages/notes/NotesPage.js
+++ src/pages/notes/NotesPage.js
@@ -16,6 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function NotesPage({ message, filter = "" }) {
   const [notes, setNotes] = useState({ results: [] });
@@ -49,7 +50,7 @@ function NotesPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
+        <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -88,7 +89,7 @@ function NotesPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
